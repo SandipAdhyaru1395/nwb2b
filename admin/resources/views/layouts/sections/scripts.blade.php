96e4561e7b11ec0cf9350e782b1dfd2277e22eac
Sandip Adhyaru <sandip@silverwebbuzz.com>
2025-09-17 14:57:27 +0530

    split vite output

--- admin/resources/views/layouts/sections/scripts.blade.php
+++ admin/resources/views/layouts/sections/scripts.blade.php
@@ -2,10 +2,10 @@
 
 @vite(['resources/assets/vendor/libs/jquery/jquery.js', 'resources/assets/vendor/js/bootstrap.js',
     ])
+@yield('vendor-script')
 
 @vite(['resources/assets/vendor/js/menu.js'])
 
-@yield('vendor-script')
 <!-- END: Page Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
