cf1d6f3c36020ab87bdfb033b8ca8750b39c0153
Sandip Adhyaru <sandip@silverwebbuzz.com>
2025-09-17 16:34:05 +0530

    bootstrap optimization

--- admin/resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js
+++ admin/resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js
@@ -1,20 +1,20 @@
-// Core libraries
-import JSZip from 'jszip';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+  // Core libraries
+  import JSZip from 'jszip';
+  import pdfMake from 'pdfmake/build/pdfmake';
+  import pdfFonts from 'pdfmake/build/vfs_fonts';
 
-// DataTables libraries (with related plugins)
-import DataTable from 'datatables.net-bs5';
-import 'datatables.net-fixedcolumns-bs5';
-import 'datatables.net-fixedheader-bs5';
-import 'datatables.net-select-bs5';
-import 'datatables.net-buttons';
-import 'datatables.net-buttons-bs5';
-import 'datatables.net-buttons/js/buttons.html5';
-import 'datatables.net-buttons/js/buttons.print';
-import 'datatables.net-responsive';
-import 'datatables.net-responsive-bs5';
-import 'datatables.net-rowgroup-bs5';
+  // DataTables libraries (with related plugins)
+  import DataTable from 'datatables.net-bs5';
+  // import 'datatables.net-fixedcolumns-bs5';
+  // import 'datatables.net-fixedheader-bs5';
+  import 'datatables.net-select-bs5';
+  // import 'datatables.net-buttons';
+  import 'datatables.net-buttons-bs5';
+  import 'datatables.net-buttons/js/buttons.html5';
+  import 'datatables.net-buttons/js/buttons.print';
+  // import 'datatables.net-responsive';
+  import 'datatables.net-responsive-bs5';
+  // import 'datatables.net-rowgroup-bs5';
 
 // This solution related to font issues with pdfMake
 pdfMake.fonts = {
