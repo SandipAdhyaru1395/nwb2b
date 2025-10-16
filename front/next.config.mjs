2d3ba8a3ce736083af80ddabcdd2ccf197069017
Sandip Adhyaru <sandip@silverwebbuzz.com>
2025-10-16 15:06:55 +0530

    Integrate PWA

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -22,7 +22,7 @@ const baseConfig = {
 }
 
 const nextConfig = withPWA({
-  dest: 'public',
+  dest: (!process.env.NEXT_PUBLIC_API_URL) ? 'public' : '/nwb2b/front/public',
   disable: (!process.env.NEXT_PUBLIC_API_URL) ? true : false,
   register: true,
   skipWaiting: true,
